35177af2c2e923dfa084063863b3433a20302a03
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-02-19 23:30:17 -0300

    updates index,tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,10 +47,28 @@ const Home = () => {
 
 export default Home
 
-export const PageContainer = styled.div``
-export const NavBar = styled.div``
+export const PageContainer = styled.div`
+        width: 100vw;
+        min-height: 100vh; 
+   
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+`
+export const NavBar = styled.div`
+    display: flex;
+    align-items: center;    
+    justify-content: space-between;
+
+    background: red;
+    max-width: 900px;
+    width: 90%;
+`
 export const Logo = styled.p``
-export const Main = styled.main``
+export const Main = styled.main`
+    max-width: 900px;
+    width: 90%`
+
 export const HeadLine = styled.h1``
 export const SubHeadLine = styled.h2``
 export const MainActions = styled.div`` 
